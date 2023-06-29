cccc75f4526b4041fe9f5f1f9d7799f8ea31727c
Romain <116175483+r0om1ain@users.noreply.github.com>
2023-06-29 19:06:36 +0200

    Delete facturation directory

--- facturation/assets/js/clients_data.js
+++
@@ -1,1 +0,0 @@
-clients='[{"name" : "BijouxShop","siren":"123 455","address1":"4 rue du Paradis","address2":"75012 Paris"},{"name":"eScape houSe","siren":"879 384","address1":"12 avenue de l\'horreur","address2":"77200 Torcy"},{"name":"OMatos","siren":"599 654","address1":"10 avenue de l\'Europe","address2":"77144 Montévrain"}]';
